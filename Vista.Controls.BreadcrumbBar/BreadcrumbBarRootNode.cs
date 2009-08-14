be133d4e18130f74e8709349026e5165662f44b0
camalot_cp <SND\camalot_cp>
2009-08-13 20:52:02 -0500

    initial checkin

---
+++ Vista.Controls.BreadcrumbBar/BreadcrumbBarRootNode.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Vista.Controls {
+  public class BreadcrumbBarRootNode : BreadcrumbBarNode {
+    internal BreadcrumbBarRootNode ( BreadcrumbBar parent, Image image )
+      : base ( string.Empty ) {
+      this.Parent = parent;
+      this.Image = image;
+    }
+
+    public Image Image { get; set; }
+
+    public Rectangle ImageBounds {
+      get {
+        return new Rectangle ( 2, 2, this.Image.Width, this.Bounds.Height );
+      }
+    }
+
+    public override Rectangle DropDownBounds {
+      get {
+        return new Rectangle ( new Point ( ImageBounds.X + this.Image.Width + 2, 0 ), new Size ( DropDownSize.Width, this.Bounds.Height ) );
+      }
+    }
+
+    internal bool IsMouseOverImage { get; set; }
+
+    protected override void OnMouseMove ( MouseEventArgs e ) {
+      base.OnMouseMove ( e );
+      this.IsMouseOverImage = Rectangle.Intersect ( new Rectangle ( this.PointToClient ( MousePosition ), new Size ( 1, 1 ) ),
+        this.ImageBounds ) != Rectangle.Empty;
+      this.Invalidate ( );
+    }
+
+    protected override void OnClick ( EventArgs e ) {
+      if ( this.IsMouseOverImage ) {
+        BreadcrumbBar bb = this.Parent as BreadcrumbBar;
+        bb.ViewMode = BreadcrumbBar.ViewModes.Text;
+      } else {
+        base.OnClick ( e );
+      }
+    }
+
+
+    protected override void OnPaint ( PaintEventArgs e ) {
+      base.OnPaint ( e );
+      Graphics g = e.Graphics;
+      if ( this.Parent != null && this.Parent is BreadcrumbBar ) {
+        BreadcrumbBar parent = this.Parent as BreadcrumbBar;
+        g.Clear ( parent.IsMouseOver ? parent.HoverBackColor : parent.BackColor );
+        int imgOffset = 3;
+        if ( this.Image != null ) {
+          DrawImage ( g );
+          imgOffset = this.Image.Width;
+        }
+
+        if ( ( this.IsMouseOverDropDown || this.IsDropDownVisible ) && this.HasChildNodes ) {
+          FillBackground ( g, DropDownBounds, new Color[ ] {
+					Color.FromArgb ( 255, 234, 246, 253 ),
+					Color.FromArgb ( 255, 215, 239, 252 ),
+					Color.FromArgb ( 255, 189, 230, 253 ),
+					Color.FromArgb ( 255, 166, 214, 244 )
+					} );
+          DrawBorder ( g, DropDownBounds, Color.FromArgb ( 200, 95, 96, 97 ) );
+        }
+        if ( this.HasChildNodes ) {
+          if ( ( ( this.IsMouseDown && this.IsMouseOverDropDown ) ) || this.IsDropDownVisible ) {
+            DrawDownArrow ( g, DropDownBounds );
+          } else {
+            DrawRightArrow ( g, DropDownBounds );
+          }
+        }
+      }
+    }
+
+    private void DrawLeftMore ( Graphics g, Rectangle rectangle ) {
+      Point startPoint = CalcStartPoint ( DropDownBounds.Size, new Size ( 8, 5 ) );
+      startPoint.Offset ( DropDownBounds.X - 1, DropDownBounds.Y );
+
+      Color clr = Color.FromArgb ( 255, 0, 0, 0 );
+      using ( Pen pen = new Pen ( clr, 1 ) ) {
+        g.DrawLine ( pen, startPoint, new Point ( startPoint.X + 7, startPoint.Y ) );
+        startPoint.Offset ( 1, 1 );
+      }
+    }
+
+    protected void DrawImage ( Graphics g ) {
+      using ( Bitmap bmp = new Bitmap ( this.Image, this.Image.Width, this.Image.Height ) ) {
+        g.DrawImage ( bmp, 2, 2, bmp.Width, bmp.Height );
+      }
+    }
+  }
+}
---
+++ Vista.Controls.BreadcrumbBar/DropDownMenuItem.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Vista.Controls {
+  public class DropDownMenuItem : ToolStripMenuItem {
+    public DropDownMenuItem ( )
+      : base ( ) {
+
+    }
+
+    public DropDownMenuItem ( string text, string path )
+      : base ( text ) {
+
+    }
+  }
+
+  public class DropDownSeparator : DropDownMenuItem {
+    public DropDownSeparator ( )
+      : base ( "-", string.Empty ) {
+
+    }
+  }
+
+  public class DropDownMenuItemCollection {
+
+    public DropDownMenuItemCollection ( BreadcrumbBarNode owner ) {
+      this.Owner = owner;
+      this.Items = new List<DropDownMenuItem> ( );
+    }
+
+    public BreadcrumbBarNode Owner { get; private set; }
+    private List<DropDownMenuItem> Items { get; set; }
+
+    public void Add ( DropDownMenuItem value ) {
+      if ( value is DropDownMenuItem ) {
+        this.Items.Add ( value );
+        this.Owner.DropDownMenu.Items.Add ( value );
+        if ( this.Items.Count == 1 ) {
+          ( this.Owner.Parent as BreadcrumbBar ).Nodes.ResizeNodes ( );
+          this.Owner.Invalidate ( );
+        }
+      }
+    }
+
+    public void AddRange ( DropDownMenuItem[ ] items ) {
+      foreach ( var item in this.Items ) {
+        this.Add ( item );
+      }
+    }
+
+    public DropDownMenuItem this[ int index ] {
+      get {
+        return this.Items[ index ];
+      }
+    }
+
+    public void Clear ( ) {
+      this.Items.Clear();
+      this.Owner.DropDownMenu.Items.Clear ( );
+      ( this.Owner.Parent as BreadcrumbBar ).Nodes.ResizeNodes ( );
+      this.Owner.Invalidate ( );
+    }
+
+    public int Count {
+      get {
+        return this.Items.Count;
+      }
+    }
+
+    public void Remove ( DropDownMenuItem value ) {
+      this.Owner.DropDownMenu.Items.Remove ( value );
+      this.Items.Remove ( value );
+    }
+
+    public void RemoveAt ( int index ) {
+      this.Owner.DropDownMenu.Items.RemoveAt ( index );
+      this.Items.RemoveAt ( index );
+    }
+  }
+}
